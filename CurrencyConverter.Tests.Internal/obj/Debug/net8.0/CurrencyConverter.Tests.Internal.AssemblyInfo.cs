f861d37418738c2d2072e5928544fb9ef2c8b8de
Uzair Arif <uzairarif22@gmail.com>
2025-03-31 18:11:39 +0800

    metadata files

--- CurrencyConverter.Tests.Internal/obj/Debug/net8.0/CurrencyConverter.Tests.Internal.AssemblyInfo.cs
+++ CurrencyConverter.Tests.Internal/obj/Debug/net8.0/CurrencyConverter.Tests.Internal.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CurrencyConverter.Tests.Internal")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f12aec025c6b0cd144b689771c343ee27644204d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e115b565977c456b78cff1dc489da2b7962879bc")]
 [assembly: System.Reflection.AssemblyProductAttribute("CurrencyConverter.Tests.Internal")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CurrencyConverter.Tests.Internal")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
